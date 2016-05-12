cf64a0c137cb2de68432c71d96d226fcde05fb32
Jacob Vigeveno <vigevenoj@gmail.com>
2016-05-12 12:45:43 -0700

    Add MQTT client health check, not wired up

---
+++ src/main/java/com/sharkbaitextraordinaire/quakes/health/MqttClientHealthCheck.java
@@ -0,0 +1,22 @@
+package com.sharkbaitextraordinaire.quakes.health;
+
+import com.codahale.metrics.health.HealthCheck;
+import com.sharkbaitextraordinaire.quakes.client.OwntracksMqttClient;
+
+public class MqttClientHealthCheck extends HealthCheck {
+  
+  private final OwntracksMqttClient client;
+
+  public MqttClientHealthCheck(OwntracksMqttClient client) {
+    this.client = client;
+  }
+
+  @Override
+  protected Result check() throws Exception {
+    if (client.getClient().isConnected()) {
+      return Result.healthy();
+    } else {
+      return Result.unhealthy("Disconnected from MQTT broker " + client.getClient().getServerURI());
+    }
+  }
+}
