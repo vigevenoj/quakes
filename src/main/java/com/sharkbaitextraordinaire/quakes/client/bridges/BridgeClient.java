98deb503435ccbf103982873bf1fcde9eef02841
Jacob Vigeveno <vigevenoj@gmail.com>
2016-05-27 00:03:43 -0700

    formatting. Bridge update class deserializes correctly when no bridge is lifted

--- src/main/java/com/sharkbaitextraordinaire/quakes/client/bridges/BridgeClient.java
+++ src/main/java/com/sharkbaitextraordinaire/quakes/client/bridges/BridgeClient.java
@@ -1,6 +1,5 @@
 package com.sharkbaitextraordinaire.quakes.client.bridges;
 
-
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 
 import java.io.IOException;
@@ -26,43 +25,41 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sharkbaitextraordinaire.quakes.BridgeClientConfiguration;
+import com.sharkbaitextraordinaire.quakes.core.BridgeUpdate;
 
 import io.dropwizard.lifecycle.Managed;
 
 public class BridgeClient implements Managed {
-	
+
 	private Client client;
 	private final Logger logger = LoggerFactory.getLogger(BridgeClient.class);
 	private BridgeClientConfiguration bridgeClientConfiguration;
 	public static final String targetUrl = "https://api.multco.us/bridges/sse";
-	
+
 	public BridgeClient(BridgeClientConfiguration bridgeClientConfiguration) {
 		this.bridgeClientConfiguration = bridgeClientConfiguration;
 	}
-	
 
-//	public void setClient(Client client) {
-//		this.client = client;
-//		client.register(SseFeature.class);
-//	}
-//	
-//	public Client getClient() {
-//		return this.client;
-//	}
+	// public void setClient(Client client) {
+	// this.client = client;
+	// client.register(SseFeature.class);
+	// }
+	//
+	// public Client getClient() {
+	// return this.client;
+	// }
 
 	@Override
 	public void start() throws Exception {
 		logger.info("Bridge lift status client starting up...");
 		logger.info("Using " + bridgeClientConfiguration.getApiURL() + " as target");
-		
-		client = ClientBuilder.newBuilder()
-				.register(SseFeature.class).build();		
-		WebTarget target = client.target(bridgeClientConfiguration.getApiURL() 
-				+ "?access_token="
-				+ bridgeClientConfiguration.getApiKey());
-		
+
+		client = ClientBuilder.newBuilder().register(SseFeature.class).build();
+		WebTarget target = client.target(
+				bridgeClientConfiguration.getApiURL() + "?access_token=" + bridgeClientConfiguration.getApiKey());
+
 		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		
+
 		EventInput eventInput = target.request().get(EventInput.class);
 		while (!eventInput.isClosed()) {
 			final InboundEvent inboundEvent = eventInput.read();
@@ -72,38 +69,40 @@ public class BridgeClient implements Managed {
 				break;
 			}
 			if (inboundEvent.getName() == "null") {
-				// this is a keep-alive message, and should be seen every 20 seconds
-				// TODO when this is working consistently, set log level back to debug
+				// this is a keep-alive message, and should be seen every 20
+				// seconds
+				// TODO when this is working consistently, set log level back to
+				// debug
 				logger.info(inboundEvent.getName());
 			} else if (inboundEvent.getName() == "bridge data") {
 				// Bridge Data:
-				// This is a json object consisting of possible updates to bridge statuses
+				// This is a json object consisting of possible updates to
+				// bridge statuses
 				logger.info(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
-				TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
-				HashMap<String,Object> o = mapper.readValue(inboundEvent.readData(), typeRef);
+				BridgeUpdate bu = mapper.readValue(inboundEvent.readData(), BridgeUpdate.class);
+				String changedBridge = bu.getChangedBridge();
+				String bridgeEventTime = "";
+				String event = "";
+						
+				if (bu.getChangedItem().equals("status")) {
+					bridgeEventTime = bu.getBridgeUpdates().get(changedBridge).getUpTime().toString();
+					event = "raised";
+				} else {
+					bridgeEventTime = bu.getBridgeUpdates().get(changedBridge).getLastFive().get(0).getDownTime().toString();
+					event = "lowered";
+				}
+				logger.error(changedBridge + " " + event + " at " + bridgeEventTime);
 
-        String changedBridge = ((HashMap<String,String>) o.get("changed")).get("bridge"); 
-        String changedStatus = ((HashMap<String,String>) o.get("changed")).get("item"); 
-
-        Boolean bridgeStatus = ((HashMap<String,Boolean>) o.get(changedBridge)).get(changedStatus);
-        if (((HashMap<String,Boolean>) o.get(changedBridge)).get("status")) {
-          String bridgeEventTime = ((HashMap<String,String>) o.get(changedBridge)).get("upTime");
-          String event = "raised";
-        } else {
-          String[] bridgeEventTime = ((HashMap<String,String[]>) o.get(changedBridge)).get("lastFive");
-          String event = "lowered";
-        }
-				
 			} else {
 				logger.info(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void stop() throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
