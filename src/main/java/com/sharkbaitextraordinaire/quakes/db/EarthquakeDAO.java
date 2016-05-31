63face1bff0df1ad6bc511de1a8923c52b8e5f30
Jacob Vigeveno <vigevenoj@gmail.com>
2016-05-30 23:40:44 -0700

    initial commit of earthquake dao

--- src/main/java/com/sharkbaitextraordinaire/quakes/db/EarthquakeDAO.java
+++ src/main/java/com/sharkbaitextraordinaire/quakes/db/EarthquakeDAO.java
@@ -9,7 +9,25 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
 public interface EarthquakeDAO {
 
+	@SqlUpdate("create table earthquakes "
+			+ "magnitude float(2,2), "
+			+ "place varchar(64), " 
+			+ "time int, "
+			+ "update int, "
+			+ "tz  int, "
+			+ "url varchar(256), "
+			+ "detail varchar(256), "
+			+ "felt varchar(10), "
+			+ "cdi varchar(10), "
+			+ "tsunami varchar(8), "
+			+ "sig int, "
+			+ "code varchar(24), "
+			+ "idsvarchar(24), "
+			+ "type varchar(32), "
+			+ "title varchar(256), "
+			+ "id varchar(32)")
 	void createTableIfNotExists();
 	
+
 	void insert();
 }
