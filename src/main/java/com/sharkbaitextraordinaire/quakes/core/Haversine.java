b53733d5fa2662f00e593f4a6dca93009c3783b1
Jacob Vigeveno <vigevenoj@gmail.com>
2016-06-05 19:18:02 -0700

    Made haversine private, made distance public static

--- src/main/java/com/sharkbaitextraordinaire/quakes/core/Haversine.java
+++ src/main/java/com/sharkbaitextraordinaire/quakes/core/Haversine.java
@@ -5,7 +5,7 @@ import org.geojson.Point;
 public class Haversine {
 	private static final int EARTH_RADIUS = 6371; //radius in km
 	
-	public double distance(Point p1, Point p2) {
+	public static double distance(Point p1, Point p2) {
 		
 		double deltaLatitude = Math.toRadians(p1.getCoordinates().getLatitude() - p2.getCoordinates().getLatitude());
 		double deltaLongitude = Math.toRadians(p1.getCoordinates().getLongitude() - p2.getCoordinates().getLongitude());
@@ -19,7 +19,7 @@ public class Haversine {
 		return EARTH_RADIUS * c; 		
 	}
 	
-	public static double haversine(double val) {
+	private static double haversine(double val) {
 		return Math.pow(Math.sin(val / 2), 2);
 	}
 }
