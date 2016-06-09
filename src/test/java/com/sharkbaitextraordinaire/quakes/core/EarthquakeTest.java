d10d16ef0a63f1eddb28d5ec798b2dd9f52fde6b
Jacob Vigeveno <vigevenoj@gmail.com>
2016-06-08 22:33:41 -0700

    Added a test to create earthquake with an integer magnitude

--- src/test/java/com/sharkbaitextraordinaire/quakes/core/EarthquakeTest.java
+++ src/test/java/com/sharkbaitextraordinaire/quakes/core/EarthquakeTest.java
@@ -39,4 +39,33 @@ public class EarthquakeTest {
 		Earthquake quake = new Earthquake(f);
 		assertNotNull(quake);
 	}
+	
+	public void TestCreateEarthquakeWithMagnitudeOne() throws Exception {
+		HashMap<String,Object> properties = new HashMap<String, Object>();
+		properties.put("mag", 1);
+		properties.put("place", "6km W of Huntington Beach, CA");
+		properties.put("time", 1465358999660l);
+		properties.put("updated", 1465359724520l);
+		properties.put("tz", -420);
+		properties.put("url", "http://earthquake.usgs.gov/earthquakes/eventpage/ci37372583");
+		properties.put("detail", "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/ci37372583.geojson");
+		properties.put("felt", 9);
+		properties.put("cdi", 4.1d);
+		properties.put("tsunami", 0);
+		properties.put("sig", 95);
+		properties.put("code", "37372583");
+		properties.put("ids", ",ci37372583,");
+		properties.put("type", "earthquake");
+		properties.put("title", "M 2.4 - 6km W of Huntington Beach, CA");
+		
+		Point p = new Point(-118.0586667,33.6868333,13.81);
+		
+		Feature f = new Feature();
+		f.setProperties(properties);
+		f.setGeometry(p);
+		f.setId("ci37372583");
+		
+		Earthquake quake = new Earthquake(f);
+		assertNotNull(quake);
+	}
 }
