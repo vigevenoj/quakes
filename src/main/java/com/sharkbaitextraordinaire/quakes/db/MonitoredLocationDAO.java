aed73b59c263132719c0d769b573c4d127cd5420
Jacob Vigeveno <vigevenoj@gmail.com>
2016-06-18 23:54:48 -0700

    Re-order monitored location parameters to match geojson Points as longitude,latitude

--- src/main/java/com/sharkbaitextraordinaire/quakes/db/MonitoredLocationDAO.java
+++ src/main/java/com/sharkbaitextraordinaire/quakes/db/MonitoredLocationDAO.java
@@ -14,12 +14,12 @@ public interface MonitoredLocationDAO {
 	@SqlUpdate("create table monitoredlocations (lat decimal(9,6), lon decimal(9,6))")
 	void createTableIfNotExists();
 	
-	@SqlQuery("select latitude, longitude from monitoredlocations")
+	@SqlQuery("select longitude, latitude from monitoredlocations")
 	List<MonitoredLocation> getAllMonitoredLocations();
 	
-	@SqlUpdate("insert into monitoredlocations (latitude, longitude) values (:latitude, :longitude)")
+	@SqlUpdate("insert into monitoredlocations (longitude, latitude) values (:longitude, latitude)")
 	void insert(@Bind("latitude") double latitude, @Bind("longitude") double longitude );
 	
-	@SqlUpdate("insert into monitoredlocations (latitude, longitude) values (l.latitude, l.longitude)")
+	@SqlUpdate("insert into monitoredlocations (longitude, latitude) values (l.longitude, l.latitude)")
 	void insert(@BindBean MonitoredLocation l);
 }
