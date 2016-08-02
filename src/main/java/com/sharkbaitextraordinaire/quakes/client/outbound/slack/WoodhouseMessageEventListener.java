f275a4089069f292d690991182a9fac15cb36b8a
Jacob Vigeveno <vigevenoj@gmail.com>
2016-08-01 23:06:33 -0700

    Some notes on future woodhouse implementation

--- src/main/java/com/sharkbaitextraordinaire/quakes/client/outbound/slack/WoodhouseMessageEventListener.java
+++ src/main/java/com/sharkbaitextraordinaire/quakes/client/outbound/slack/WoodhouseMessageEventListener.java
@@ -10,18 +10,21 @@ import allbegray.slack.webapi.SlackWebApiClient;
 
 public class WoodhouseMessageEventListener implements EventListener {
 
+
 	private final Logger logger = LoggerFactory.getLogger(WoodhouseMessageEventListener.class);
 	SlackWebApiClient slackClient; // convenience for posting messages
-	
+
 	public WoodhouseMessageEventListener(SlackWebApiClient slackClient) {
 		this.slackClient = slackClient;
 	}
-	
+
 	@Override
 	public void handleMessage(JsonNode message) {
 		String messageText = message.get("text").textValue();
-		String messageChannel = message.get("channel").textValue();
-		
+//		String messageChannel = message.get("channel").textValue();
+//		String messageType = message.get("type").textValue();
+//		String user = message.get("user").textValue();
+
 		if (messageText.startsWith("woodhouse")) { // TODO or if this mentions the integration user
 			logger.warn("Starting woodhouse integration command for '" + messageText + "'");
 			handleWoodhouseIntegration(message);
@@ -30,9 +33,25 @@ public class WoodhouseMessageEventListener implements EventListener {
 
 	private void handleWoodhouseIntegration(JsonNode message) {
 		// TODO this isn't written yet
+		if (message.get("text").textValue().matches("(?i).*bus me home.*")) {
+			handleBusMeHomeRequest(message);
+		}
 	}
 
-  private void handleBusMeHomeRequest() {
-    // TODO trimet intgration required
-  }
+	private void handleBusMeHomeRequest(JsonNode message) {
+		// check current user, we might not have a location for them
+		// if this user is location-enabled,
+		isRequestingUserLocationEnabled(message.get("user").textValue());
+		// get user's current location
+		// TODO we need to get a LocationDAO to call findLatest on 
+		// get trimet configuration
+		// get home location
+		// calculate haversine distance between user and home
+		// tell the user how to get home
+	}
+	
+	protected boolean isRequestingUserLocationEnabled(String username) {
+		// TODO store this somewhere
+		return true;
+	}
 }
